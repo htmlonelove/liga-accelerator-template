33a4ee709f1d4caafce66162a141da9be5313ca7
justforthesoul <46541413+justforthesoul@users.noreply.github.com>
2022-02-02 15:10:25 +0200

    Синхронизирует изменения со сборкой на pug

---
+++ gulpfile.js
@@ -0,0 +1,147 @@
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const sourcemap = require('gulp-sourcemaps');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const server = require('browser-sync').create();
+const csso = require('gulp-csso');
+const rename = require('gulp-rename');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const svgstore = require('gulp-svgstore');
+const del = require('del');
+const webpackStream = require('webpack-stream');
+const webpackConfig = require('./webpack.config.js');
+const gcmq = require('gulp-group-css-media-queries');
+
+const css = () => {
+  return gulp.src('source/sass/style.scss')
+      .pipe(plumber())
+      .pipe(sourcemap.init())
+      .pipe(sass())
+      .pipe(postcss([autoprefixer({
+        grid: true,
+      })]))
+      .pipe(gcmq()) // выключите, если в проект импортятся шрифты через ссылку на внешний источник
+      .pipe(gulp.dest('build/css'))
+      .pipe(csso())
+      .pipe(rename('style.min.css'))
+      .pipe(sourcemap.write('.'))
+      .pipe(gulp.dest('build/css'))
+      .pipe(server.stream());
+};
+
+const js = () => {
+  return gulp.src(['source/js/main.js'])
+      .pipe(webpackStream(webpackConfig))
+      .pipe(gulp.dest('build/js'))
+};
+
+const svgo = () => {
+  return gulp.src('source/img/**/*.{svg}')
+      .pipe(imagemin([
+        imagemin.svgo({
+            plugins: [
+              {removeViewBox: false},
+              {removeRasterImages: true},
+              {removeUselessStrokeAndFill: false},
+            ]
+          }),
+      ]))
+      .pipe(gulp.dest('source/img'));
+};
+
+const sprite = () => {
+  return gulp.src('source/img/sprite/*.svg')
+      .pipe(svgstore({inlineSvg: true}))
+      .pipe(rename('sprite_auto.svg'))
+      .pipe(gulp.dest('build/img'));
+};
+
+const copySvg = () => {
+  return gulp.src('source/img/**/*.svg', {base: 'source'})
+      .pipe(gulp.dest('build'));
+};
+
+const copyImages = () => {
+  return gulp.src('source/img/**/*.{png,jpg,webp}', {base: 'source'})
+      .pipe(gulp.dest('build'));
+};
+
+const copy = () => {
+  return gulp.src([
+    'source/**.html',
+    'source/fonts/**',
+    'source/img/**',
+    'source/favicon/**',
+  ], {
+    base: 'source',
+  })
+      .pipe(gulp.dest('build'));
+};
+
+const clean = () => {
+  return del('build');
+};
+
+const syncServer = () => {
+  server.init({
+    server: 'build/',
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false,
+  });
+
+  gulp.watch('source/**.html', gulp.series(copy, refresh));
+  gulp.watch('source/sass/**/*.{scss,sass}', gulp.series(css));
+  gulp.watch('source/js/**/*.{js,json}', gulp.series(js, refresh));
+  gulp.watch('source/data/**/*.{js,json}', gulp.series(copy, refresh));
+  gulp.watch('source/img/**/*.svg', gulp.series(copySvg, sprite, refresh));
+  gulp.watch('source/img/**/*.{png,jpg,webp}', gulp.series(copyImages, refresh));
+
+  gulp.watch('source/favicon/**', gulp.series(copy, refresh));
+  gulp.watch('source/video/**', gulp.series(copy, refresh));
+  gulp.watch('source/downloads/**', gulp.series(copy, refresh));
+  gulp.watch('source/*.php', gulp.series(copy, refresh));
+};
+
+const refresh = (done) => {
+  server.reload();
+  done();
+};
+
+const build = gulp.series(clean, svgo, copy, css, sprite, js);
+
+const start = gulp.series(build, syncServer);
+
+// Optional tasks
+//---------------------------------
+
+// Используйте отличное от дефолтного значение root, если нужно обработать отдельную папку в img,
+// а не все изображения в img во всех папках.
+
+// root = '' - по дефолту webp добавляются и обналяются во всех папках в source/img/
+// root = 'content/' - webp добавляются и обновляются только в source/img/content/
+
+const createWebp = () => {
+  const root = '';
+  return gulp.src(`source/img/${root}**/*.{png,jpg}`)
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest(`source/img/${root}`));
+};
+
+const optimizeImages = () => {
+  return gulp.src('build/img/**/*.{png,jpg}')
+      .pipe(imagemin([
+        imagemin.optipng({optimizationLevel: 3}),
+        imagemin.mozjpeg({quality: 75, progressive: true}),
+      ]))
+      .pipe(gulp.dest('build/img'));
+};
+
+exports.imagemin = optimizeImages;
+exports.webp = createWebp;
+exports.start = start;
+exports.build = build;
