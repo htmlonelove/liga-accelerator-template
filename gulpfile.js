ba97edf683a38b47694db577468dbc8e281453c6
SokolovMichael <unknownpersons@mail.ru>
2022-07-20 00:52:57 +0300

    Подготовка проекта

--- gulpfile.js
+++ gulpfile.js
@@ -88,7 +88,7 @@ const clean = () => {
 const syncServer = () => {
   server.init({
     server: 'build/',
-    index: 'sitemap.html',
+    index: 'index.html',
     notify: false,
     open: true,
     cors: true,
