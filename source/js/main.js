99e7cfa65e2adeedf6fb4c85275e80ddc36d9693
justforthesoul <46541413+justforthesoul@users.noreply.github.com>
2022-10-17 15:22:26 +0200

    Добавляет возможность использования nodejs v16, добавляет миксины интерактивных состояний, добавляет focus-visible-polyfill

--- source/js/main.js
+++ source/js/main.js
@@ -28,12 +28,12 @@ window.addEventListener('DOMContentLoaded', () => {
 
 // вместо модификаторов .block--active используем утилитарные классы
 // .is-active || .is-open || .is-invalid и прочие (обязателен нейминг в два слова)
-// .select.select--opened ❌ ---> [data-select].is-open ✔️
+// .select.select--opened ❌ ---> [data-select].is-open ✅
 
 // выносим все в дата атрибуты
 // url до иконок пинов карты, настройки автопрокрутки слайдера, url к json и т.д.
 
-// для адаптивного JS используейтся matchMedia и addListener
+// для адаптивного JS используется matchMedia и addListener
 // const breakpoint = window.matchMedia(`(min-width:1024px)`);
 // const breakpointChecker = () => {
 //   if (breakpoint.matches) {
