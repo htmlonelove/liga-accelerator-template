285e0129b2b007f0363de011ba3439306e6abf94
luna100500 <ermakova.ab@gmail.com>
2022-10-19 01:03:00 +0100

    создана разметка

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,45 @@
-import gulp from 'gulp';
-import browserSync from 'browser-sync';
-import del from 'del';
-import compileStyles from './gulp/compileStyles.mjs';
-import { copy, copyImages, copySvg } from './gulp/copyAssets.mjs';
-import js from './gulp/compileScripts.mjs';
-import { svgo, sprite, createWebp, optimizeImages } from './gulp/optimizeImages.mjs';
+import gulp from "gulp";
+import browserSync from "browser-sync";
+import del from "del";
+import compileStyles from "./gulp/compileStyles.mjs";
+import { copy, copyImages, copySvg } from "./gulp/copyAssets.mjs";
+import js from "./gulp/compileScripts.mjs";
+import {
+  svgo,
+  sprite,
+  createWebp,
+  optimizeImages,
+} from "./gulp/optimizeImages.mjs";
 
 const server = browserSync.create();
 const streamStyles = () => compileStyles().pipe(server.stream());
 
-const clean = () => del('build');
+const clean = () => del("build");
 
 const syncServer = () => {
   server.init({
-    server: 'build/',
-    index: 'sitemap.html',
+    server: "build/",
+    index: "index.html",
     notify: false,
     open: true,
     cors: true,
     ui: false,
   });
 
-  gulp.watch('source/**.html', gulp.series(copy, refresh));
-  gulp.watch('source/sass/**/*.{scss,sass}', streamStyles);
-  gulp.watch('source/js/**/*.{js,json}', gulp.series(js, refresh));
-  gulp.watch('source/data/**/*.{js,json}', gulp.series(copy, refresh));
-  gulp.watch('source/img/**/*.svg', gulp.series(copySvg, sprite, refresh));
-  gulp.watch('source/img/**/*.{png,jpg,webp}', gulp.series(copyImages, refresh));
+  gulp.watch("source/**.html", gulp.series(copy, refresh));
+  gulp.watch("source/sass/**/*.{scss,sass}", streamStyles);
+  gulp.watch("source/js/**/*.{js,json}", gulp.series(js, refresh));
+  gulp.watch("source/data/**/*.{js,json}", gulp.series(copy, refresh));
+  gulp.watch("source/img/**/*.svg", gulp.series(copySvg, sprite, refresh));
+  gulp.watch(
+    "source/img/**/*.{png,jpg,webp}",
+    gulp.series(copyImages, refresh)
+  );
 
-  gulp.watch('source/favicon/**', gulp.series(copy, refresh));
-  gulp.watch('source/video/**', gulp.series(copy, refresh));
-  gulp.watch('source/downloads/**', gulp.series(copy, refresh));
-  gulp.watch('source/*.php', gulp.series(copy, refresh));
+  gulp.watch("source/favicon/**", gulp.series(copy, refresh));
+  gulp.watch("source/video/**", gulp.series(copy, refresh));
+  gulp.watch("source/downloads/**", gulp.series(copy, refresh));
+  gulp.watch("source/*.php", gulp.series(copy, refresh));
 };
 
 const refresh = (done) => {
