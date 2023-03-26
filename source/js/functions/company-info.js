c1bd2505a2f832fb369ad4902fefa6196e80529e
Siestaa <lex.qz97@gmail.com>
2023-03-26 14:19:37 +0500

    fix errors

--- source/js/functions/company-info.js
+++ source/js/functions/company-info.js
@@ -4,9 +4,17 @@ const text = [...document.querySelectorAll('[data-text="company"]')];
 const openCompanyInfo = () => {
   text.map((item) => {
     if (item.dataset.hide === 'desktop') {
-      item.classList.contains('company__text-item--hide') ? item.classList.remove('company__text-item--hide') : item.classList.add('company__text-item--hide');
+      if (item.classList.contains('company__text-item--hide')) {
+        item.classList.remove('company__text-item--hide');
+      } else {
+        item.classList.add('company__text-item--hide');
+      }
     } else if (item.dataset.hide === 'mobile') {
-      item.classList.contains('company__text-item--hide-mobile') ? item.classList.remove('company__text-item--hide-mobile') : item.classList.add('company__text-item--hide-mobile') ;
+      if (item.classList.contains('company__text-item--hide-mobile')) {
+        item.classList.remove('company__text-item--hide-mobile');
+      } else {
+        item.classList.add('company__text-item--hide-mobile');
+      }
     }
 
     if (button.textContent === 'Подробнее') {
