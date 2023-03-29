ce020064a2a1db769450f053b4ffac65342b88df
Siestaa <lex.qz97@gmail.com>
2023-03-30 00:03:28 +0500

    redy for test

--- source/js/functions/popup.js
+++ source/js/functions/popup.js
@@ -1,13 +1,21 @@
 const buttons = [...document.querySelectorAll('[data-button="popup"')];
 const popup = document.querySelector('[data-id="popup"]');
 
-const checkPopup = () =>
+const checkPopup = () => {
   popup.classList.toggle('popup--hide');
+  popup.querySelector('input').focus();
+};
 
 const popupOn = () => {
   buttons.map((item) => item.addEventListener('click', checkPopup));
   document.addEventListener('keydown', (evt) => {
     if (evt.key === 'Escape') {
+      popup.classList.add('popup--hide');
+    }
+  });
+  document.addEventListener('click', (e) => {
+    const target = e.target;
+    if (!target.closest('.popup__container') && !target.classList.contains('header__button')) {
       popup.classList.add('popup--hide');
     }
   });
