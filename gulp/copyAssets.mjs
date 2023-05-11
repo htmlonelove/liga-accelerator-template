80523c6caa4536f3a4eba51f062efda84991788f
Nikita Naumov <naumovn808@gmail.com>
2023-05-11 22:21:25 +0500

    init: start of work on project

--- gulp/copyAssets.mjs
+++ gulp/copyAssets.mjs
@@ -13,7 +13,8 @@ const copy = () =>
     'source/**.html',
     'source/fonts/**',
     'source/img/**',
-    'source/favicon/**'
+    'source/favicon.ico',
+    'source/manifest.json'
   ], {
     base: 'source',
   })
