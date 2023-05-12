2358d5560f174915b0d7d0b5066eeb9b57c92d42
Nikita Naumov <naumovn808@gmail.com>
2023-05-13 00:21:44 +0500

    feat: add section trainers

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,7 @@
-import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
-import {Form} from './modules/form-validate/form';
-
+import { iosVhFix } from './utils/ios-vh-fix';
+import { initModals } from './modules/modals/init-modals';
+import { Form } from './modules/form-validate/form';
+import './modules/video';
 // ---------------------------------
 
 function changeAddElements() {
