882b8166cd78c0a79ae7aca8c32b1fad6dc1d4bf
Nikita Naumov <naumovn808@gmail.com>
2023-05-17 23:16:00 +0500

    feat: section swiper is done

---
+++ source/js/modules/init-swiper.js
@@ -0,0 +1,92 @@
+import Swiper, {Navigation} from '../../../node_modules/swiper/swiper-bundle';
+Swiper.use([Navigation]);
+
+
+const trainersSlider = document.querySelector('.swiper-container');
+
+const initTrainersSlider = () => {
+
+  if (trainersSlider) {
+
+    // eslint-disable-next-line no-unused-vars
+    const mySwiper = new Swiper('.swiper__slider', {
+      direction: 'horizontal',
+      loop: true,
+      slidesPerView: 'auto',
+      // spaceBetween: 30,
+      // initialSlide: 2,
+      // centeredSlides: true,
+      autoResize: false,
+      centeredSlides: false,
+      // loopedSlides: 0,
+      // maxBackfaceHiddenSlides: 0,
+      // keyboard: {
+      //   enabled: true,
+      //   onlyInViewport: true,
+      // },
+      breakpoints: {
+        1280: {
+          slidesPerView: 4,
+          slidesPerGroup: 4,
+          centeredSlides: false,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 3,
+          slidesPerGroup: 3,
+          spaceBetween: 30,
+          centeredSlides: false,
+        },
+        580: {
+          slidesPerView: 2,
+          slidesPerGroup: 2,
+          spaceBetween: 30,
+          centeredSlides: false,
+        },
+        320: {
+          slidesPerView: 1,
+          slidesPerGroup: 1,
+          spaceBetween: 30,
+          centeredSlides: false,
+        },
+      },
+
+      // breakpoints: {
+      // when window width is >= 320px
+      //   320: {
+      //     slidesPerView: 1,
+      //     slidesPerGroup: 1,
+      //     centeredSlides: false,
+      //   },
+      // }
+      //   // when window width is >= 768px
+      //   768: {
+      //     slidesPerView: 2,
+      //     slidesPerGroup: 2,
+      //     spaceBetween: 30,
+      //     centeredSlides: false,
+      //   },
+      //   // when window width is >= 1200px
+      //   // 1200: {
+      //   //   slidesPerView: 3,
+      //   //   slidesPerGroup: 3,
+      //   //   centeredSlides: false,
+      //   // },
+      //   // // when window width is >= 1200px
+      //   1280: {
+      //     slidesPerView: 4,
+      //     slidesPerGroup: 4,
+      //     centeredSlides: false,
+      //   },
+      // },
+
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  }
+
+};
+
+export {initTrainersSlider};
